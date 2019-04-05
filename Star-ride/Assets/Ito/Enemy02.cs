9fa10e68b5c21f91972799d941a200ecfde1fa90
ItoLuka <legnadaed277@gmail.com>
2019-04-05 15:39:49 +0900

    少し変更

--- Star-ride/Assets/Ito/Enemy02.cs
+++ Star-ride/Assets/Ito/Enemy02.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy02 : MonoBehaviour
 {
     public Transform target;    //追いかける対象 -オブジェクトをインスペクタから登録できるように
+    public GameObject enemy;    
     public float speed = 0.1f;  //移動スピード
     private Vector3 vec;
 
@@ -23,8 +24,20 @@ public class Enemy02 : MonoBehaviour
         ////targetに向かって進む
         //transform.position += transform.forward * speed;
 
+        //targetに向かって進む
         this.transform.position = Vector2.MoveTowards(this.transform.position, 
             new Vector2(target.transform.position.x, target.transform.position.y), speed * Time.deltaTime);
 
+        // 場外といいたい
+        Vector3 field = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 pos = transform.position;
+
+        // 上手く比較ができなかった
+        // のでプレイヤーに接触したら消える状態　仮置き
+        if (pos == this.target.transform.position)
+        {
+            // 表示（OFF）
+            this.enemy.SetActive(false);
+        }
     }
 }
