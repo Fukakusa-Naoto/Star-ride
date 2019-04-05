1f967e970d76b0704bf48f6492b7fe80de9be799
Fukakusa Naoto <fukakusa.naoto0024@gmail.com>
2019-04-05 14:03:39 +0900

    プログラム整理中

--- Star-ride/Assets/Fuakaksa/Scripts/PlayerUIController_F.cs
+++ Star-ride/Assets/Fuakaksa/Scripts/PlayerUIController_F.cs
@@ -1,70 +1,105 @@
-﻿using System.Collections;
+﻿//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+//! @file		PlayerUIController_F.cs
+//!
+//! @summary	プレイヤーを制御するUIの制御に関するC#スクリプト
+//!
+//! @date		2019.04.05
+//!
+//! @author		深草直斗
+//__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/__/
+
+// 名前空間の省略 ===========================================================
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// クラスの定義 =============================================================
 public class PlayerUIController_F : MonoBehaviour
 {
+	// <メンバ定数>
 	// 最大付与力量
-	private const float MaxMagnitude = 5.0f;
+	private const float MAX_MAGNITUDE = 5.0f;
 
+	// <メンバ変数>
 	// 溜めた値
-	private float m_sinkValue=0;
+	private float m_sinkValue = 0;
 	// 送る力
 	private Vector2 m_sendForce = Vector2.zero;
-
-	// 発射方向
-	[SerializeField]
-	private Vector3 m_direction = Vector3.zero;
-
 	// 発射方向の力
 	private Vector3 m_currentForce = Vector3.zero;
-
 	// メインカメラ
 	private Camera m_mainCamera = null;
-
 	// メインカメラ座標
 	private Transform m_mainCameraTransform = null;
-
 	// ドラッグ開始点
 	private Vector3 m_dragStart = Vector3.zero;
 
 
-	// Start is called before the first frame update
+	// メンバ関数の定義 =====================================================
+	//--------------------------------------------------------------------
+	//! @summary   初期化処理
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//--------------------------------------------------------------------
 	void Start()
     {
+		// カメラの取得
         m_mainCamera = Camera.main;
+		// カメラの座標情報の取得
         m_mainCameraTransform = m_mainCamera.transform;
     }
 
 
+
+	//--------------------------------------------------------------------
+	//! @summary   更新処理
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//--------------------------------------------------------------------
 	private void FixedUpdate()
 	{
+		// 送る力をリセットする
 		m_sendForce = Vector2.zero;
 	}
 
 
-	/// <summary>
-	/// マウス座標をワールド座標に変換して取得
-	/// </summary>
-	/// <returns></returns>
+
+	//--------------------------------------------------------------------
+	//! @summary   マウス座標をワールド座標に変換して取得
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    変換後の座標
+	//--------------------------------------------------------------------
 	private Vector3 GetMousePosition()
 	{
 		// マウスから取得できないZ座標を補完する
-		var position = Input.mousePosition;
+		Vector3 position = Input.mousePosition;
 		position.z = m_mainCameraTransform.position.z;
 		position = m_mainCamera.ScreenToWorldPoint(position);
-		position.z = 0;
+
+		// Z座標は使わないので0にする
+		position.z = 0.0f;
 
 		return position;
 	}
 
-	/// <summary>
-	/// ドラック開始イベントハンドラ
-	/// </summary>
+
+
+	//--------------------------------------------------------------------
+	//! @summary   ドラック開始イベントハンドラ
+	//!
+	//! @parameter [void] なし
+	//!
+	//! @return    なし
+	//--------------------------------------------------------------------
 	public void OnMouseDown()
 	{
-		Debug.Log("ドラック開始イベント");
-
 		m_dragStart = this.GetMousePosition();
 	}
 
@@ -74,14 +109,12 @@ public class PlayerUIController_F : MonoBehaviour
 	public void OnMouseDrag()
 	{
 		var position = this.GetMousePosition();
-		Debug.Log(position);
 
 		m_currentForce = position - m_dragStart;
-		if (m_currentForce.magnitude > MaxMagnitude * MaxMagnitude)
+		if (m_currentForce.magnitude > MAX_MAGNITUDE * MAX_MAGNITUDE)
 		{
-			m_currentForce *= MaxMagnitude / m_currentForce.magnitude;
+			m_currentForce *= MAX_MAGNITUDE / m_currentForce.magnitude;
 		}
-		Debug.Log(m_currentForce);
 	}
 
 	/// <summary>
@@ -89,15 +122,12 @@ public class PlayerUIController_F : MonoBehaviour
 	/// </summary>
 	public void OnMouseUp()
 	{
-		Debug.Log("ドラック終了イベント");
-
 		m_sendForce = m_currentForce;
 	}
 
 
 	public Vector2 GetSendForce()
 	{
-		//Debug.Log(m_sendForce);
 		return m_sendForce;
 	}
 }
