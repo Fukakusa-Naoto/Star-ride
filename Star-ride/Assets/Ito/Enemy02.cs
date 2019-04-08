d2d55fa1f3ea4d493fa7ac472e82f3c2b157b7cc
ItoLuka <legnadaed277@gmail.com>
2019-04-08 09:33:40 +0900

    enemy02追加

---
+++ Star-ride/Assets/Ito/Enemy02.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy02 : MonoBehaviour
+{
+    public Transform target;    //追いかける対象 -オブジェクトをインスペクタから登録できるように
+    public GameObject enemy;
+    public float speed = 0.1f;  //移動スピード
+    private Vector3 vec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = Quaternion.Slerp(transform.rotation,
+        //    Quaternion.LookRotation(target.position - transform.position), 0.3f);
+
+        ////targetに向かって進む
+        //transform.position += transform.forward * speed;
+
+        //targetに向かって進む
+        this.transform.position = Vector2.MoveTowards(this.transform.position,
+            new Vector2(target.transform.position.x, target.transform.position.y), speed * Time.deltaTime);
+
+        // 場外といいたい
+        Vector3 field = new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 pos = transform.position;
+
+        // 上手く比較ができなかった
+        // のでプレイヤーに接触したら消える状態　仮置き
+        if (pos == this.target.transform.position)
+        {
+            // 表示（OFF）
+            this.enemy.SetActive(false);
+        }
+    }
+}
