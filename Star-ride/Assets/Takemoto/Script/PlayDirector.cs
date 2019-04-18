d6e52b628b183a56f52adb40da7f8b6d5a615b6e
Takemoto Mayumi <0wd7-048329018m@ezweb.ne.jp>
2019-04-18 19:10:39 +0900

    チェンジスプライトのスクリプトを削除、titlestartを追加して画像を拡縮

--- Star-ride/Assets/Takemoto/Script/PlayDirector.cs
+++ Star-ride/Assets/Takemoto/Script/PlayDirector.cs
@@ -5,11 +5,18 @@ public class PlayDirector : MonoBehaviour
 {
     public AudioClip startse;
     AudioSource aud;
+    // 拡大
+    float x = 0.01f;
+    float y = 0.01f;
+
+    // 経過時間
+    float seconds;
      
     // Start is called before the first frame update
     void Start()
     {
         aud = GetComponent<AudioSource>();
+        seconds = 0f;
     }
 
     // Update is called once per frame
@@ -29,10 +36,15 @@ public class PlayDirector : MonoBehaviour
             {
                 GameObject obj = aCollider2d.transform.gameObject;
             }
-            
+
             // ""の関数を呼び出す
-            Invoke("DelayMethod", 2f);
+            Invoke("DelayMethod", 1f);
+            
         }
+        Scale(x, y);
+       
+        // 経過時間をカウント
+        seconds += Time.deltaTime;
     }
 
     // 2秒後にゲームシーンに移行
@@ -41,5 +53,26 @@ public class PlayDirector : MonoBehaviour
         SceneManager.LoadScene("GameScene");
     }
     
+    
+    void Scale(float x, float y)
+    {
+        // 始まってすぐ拡大
+        if(seconds <= 1.5f)
+        {
+            gameObject.transform.localScale += new Vector3(x, y);
+        }
 
+        // 2秒後に画像を縮小
+        if (seconds >= 1.5f)
+        {
+            // 縮小
+            gameObject.transform.localScale -= new Vector3(x, y);
+        }
+        
+        // 4秒経過で0秒に戻す
+        if(seconds >= 3f)
+        {
+            seconds = 0f;
+        }
+    }
 }
