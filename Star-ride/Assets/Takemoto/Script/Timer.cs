50b2bb406e36b452747896201b766d87a1af94fa
Takemoto Mayumi <0wd7-048329018m@ezweb.ne.jp>
2019-05-16 11:58:55 +0900

    カウントダウンのスクリプト追加

---
+++ Star-ride/Assets/Takemoto/Script/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<UnityEngine.UI.Text>().text = "00";
+        
+        // 初期値を60秒に設定
+        timer = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1秒ごとに１ずつ減らす
+        timer -= Time.deltaTime;
+
+       // GetComponent<UnityEngine.UI.Text>().text = ((float)timer).ToString();
+
+        // マイナス表示しない
+        if(timer < 0)
+        {
+            timer = 0;
+            GetComponent<Text>().text = ((float)timer).ToString();
+        }
+    }
+}
