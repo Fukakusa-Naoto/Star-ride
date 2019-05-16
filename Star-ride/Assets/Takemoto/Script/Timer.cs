069468522a22879f6c83673ee351e4165741cad6
Takemoto Mayumi <0wd7-048329018m@ezweb.ne.jp>
2019-05-16 16:01:13 +0900

    下画面UIに変更

--- Star-ride/Assets/Takemoto/Script/Timer.cs
+++ Star-ride/Assets/Takemoto/Script/Timer.cs
@@ -5,30 +5,26 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    float timer;
+   public float timer;
+   public Text timerText;
+    int second;
 
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<UnityEngine.UI.Text>().text = "00";
         
-        // 初期値を60秒に設定
-        timer = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // 1秒ごとに１ずつ減らす
-        timer -= Time.deltaTime;
+       
+            // 1秒ごとに１ずつ減らす
+            timer -= Time.deltaTime;
+        
 
-       // GetComponent<UnityEngine.UI.Text>().text = ((float)timer).ToString();
-
-        // マイナス表示しない
-        if(timer < 0)
-        {
-            timer = 0;
-            GetComponent<Text>().text = ((float)timer).ToString();
-        }
+        second = (int)timer;
+        timerText.text = second.ToString();
+        
     }
 }
